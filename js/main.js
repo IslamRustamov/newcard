012ad2d43d7793ed6cbb8b07eb3d90b894c9ac4c
Islam <islamrustamov@rambler.ru>
2019-12-02 00:45:51 +0300

    Change pic loading

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
-$(document).ready(function(){$("#btn1").click(function(){$("body").css("background-image","url(pics/asap.jpg)"),$("#head").hide(),$("#info1").fadeIn()}),$("#btn2").click(function(){$("body").css("background-image","url(pics/travis.jpg)"),$("#info1").hide(),$("#head").fadeIn()}),$("#btn3").click(function(){$("body").css("background-image","url(pics/drake.jpg)"),$("#info1").hide(),$("#info2").fadeIn()}),$("#btn4").click(function(){$("body").css("background-image","url(pics/asap.jpg)"),$("#info2").hide(),$("#info1").fadeIn()}),$("#btn5").click(function(){$("body").css("background-image","url(pics/travis1.jpg)"),$("#info2").hide(),$("#foot").fadeIn()}),$("#btn6").click(function(){$("body").css("background-image","url(pics/drake.jpg)"),$("#foot").hide(),$("#info2").fadeIn()})});
+$(document).ready(function() {
+  $("#btn1").click(function() {
+    $("body").css(
+      "background-image",
+      "url(https://sun9-65.userapi.com/c857128/v857128208/72234/BFW-3yWc1do.jpg)"
+    );
+    $("#head").hide();
+    $("#info1").fadeIn();
+  });
+
+  $("#btn2").click(function() {
+    $("body").css(
+      "background-image",
+      "url(https://sun9-31.userapi.com/c857128/v857128208/72220/UAKxP80C4YM.jpg)"
+    );
+    $("#info1").hide();
+    $("#head").fadeIn();
+  });
+
+  $("#btn3").click(function() {
+    $("body").css(
+      "background-image",
+      "url(https://sun9-62.userapi.com/c857128/v857128208/7223e/n5h5n2lPd3Y.jpg)"
+    );
+    $("#info1").hide();
+    $("#info2").fadeIn();
+  });
+
+  $("#btn4").click(function() {
+    $("body").css(
+      "background-image",
+      "url(https://sun9-65.userapi.com/c857128/v857128208/72234/BFW-3yWc1do.jpg)"
+    );
+    $("#info2").hide();
+    $("#info1").fadeIn();
+  });
+
+  $("#btn5").click(function() {
+    $("body").css(
+      "background-image",
+      "url(https://sun9-38.userapi.com/c857128/v857128208/7222a/kk21_wC_WrY.jpg)"
+    );
+    $("#info2").hide();
+    $("#foot").fadeIn();
+  });
+
+  $("#btn6").click(function() {
+    $("body").css(
+      "background-image",
+      "url(https://sun9-62.userapi.com/c857128/v857128208/7223e/n5h5n2lPd3Y.jpg)"
+    );
+    $("#foot").hide();
+    $("#info2").fadeIn();
+  });
+});
